c87615b84bd02ffa49682d4af8916322d8741d4a
Jaime <jaimeserranovazquez0@gmail.com>
2025-11-02 21:36:31 -0600

    chore: initial spring boot project setup

---
+++ src/main/java/com/merch/MerchantOS/MerchantOsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.merch.MerchantOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantOsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchantOsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/merch/MerchantOS/MerchantOsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merch.MerchantOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantOsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
